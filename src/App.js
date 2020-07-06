3e10a56408fef229d8b98d7beccccf4ebe8077c3
Sanviiz <sanviiz06@hotmail.com>
2020-07-06 12:58:37 +0700

    Try Import Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { Button } from "@material-ui/core";
+import Header from "./components/fragments/Header";
+import Menu from "./components/fragments/Menu";
+import Login from "./components/pages/Login";
 
 export default function App() {
-  return <div>
-      
-  </div>;
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Login />
+    </div>
+  );
 }
