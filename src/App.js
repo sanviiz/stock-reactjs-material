ecc16c029ef68f4be3a1c50bb7f4863cd0703f86
Sanviiz <sanviiz06@hotmail.com>
2020-07-06 13:18:34 +0700

    Use react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import { Button } from "@material-ui/core";
 import Header from "./components/fragments/Header";
 import Menu from "./components/fragments/Menu";
 import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
 export default function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <Menu />
-      <Login />
-    </div>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/Register" component={Register} />
+      </Switch>
+    </Router>
   );
 }
