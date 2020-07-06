f985406e63ff56ed1ee0e0d7c7feaffd23117e1e
Sanviiz <sanviiz06@hotmail.com>
2020-07-06 15:00:35 +0700

    Header, Menu and Login updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, Container } from "@material-ui/core";
 import Header from "./components/fragments/Header";
 import Menu from "./components/fragments/Menu";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
+import { makeStyles } from "@material-ui/core/styles";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
 import {
   BrowserRouter as Router,
   Route,
@@ -11,15 +14,52 @@ import {
   Switch,
 } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "center",
+    padding: theme.spacing(20),
+  },
+}));
+
 export default function App() {
+  const classes = useStyles();
+  const [openDrawer, setOpenDrawer] = React.useState(true);
+
+  const handleDrawerClose = () => {
+    setOpenDrawer(false);
+  };
+
+  const handleDrawerOpen = () => {
+    setOpenDrawer(true);
+  };
+
   return (
     <Router>
-      <Header />
-      <Menu />
-      <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/Register" component={Register} />
-      </Switch>
+      <Header handleDrawerOpen={handleDrawerOpen} open={openDrawer} />
+      <Menu open={openDrawer} handleDrawerClose={handleDrawerClose} />
+      <Container className={classes.content}>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+          <Route
+            exact={true}
+            path="/"
+            component={() => <Redirect to="/login" />}
+          />
+        </Switch>
+      </Container>
     </Router>
   );
 }
